70584799a55cf9a526e23f6e51de5eec54673db7
tf-metadata-team <no-reply@google.com>
2020-07-29 16:11:39 -0700

    no-op
    PiperOrigin-RevId: 323889429

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only'
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Software Development',
